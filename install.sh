a8cf1f808b40ffc8322559be1ac600a39dc4329b
Erik Kristensen <erik@erikkristensen.com>
2014-04-09 19:04:21 -0400

    adding tzworks for SIFT

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Foremost Config
 cp foremost/foremost.conf /etc/foremost.conf
+
+# Sorter
 cp sorter/* /usr/share/tsk/sorter
+
+# Samba
 cp samba/* /etc/samba
+
+# TZWorks
+mkdir -p /usr/local/share/tzworks
+cp tzworks/bin/* /usr/local/bin
+cp -R tzworks/docs/* /usr/local/share/tzworks
