fdfffa194c38b80c2565daa5632d1236e21eb828
Erik Kristensen <erik@erikkristensen.com>
2014-08-28 16:08:37 -0600

    installs page_brute, resolves https://github.com/sans-dfir/sift/issues/28

--- install.sh
+++ install.sh
@@ -29,6 +29,9 @@ cp densityscout/densityscout /usr/local/bin
 # Intall PE Carver
 cp pe_carver/*.py /usr/local/bin
 
+# Install Page Brute
+cp page_brute/*.py /usr/local/bin
+
 # Removing old tzworks id app
 # Now called id64, installed by line #14
 if [ -e /usr/local/bin/id ]; then
