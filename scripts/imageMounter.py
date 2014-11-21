af996f594bd249fcb5614396192c1bf708947c7e
Erik Kristensen <erik@erikkristensen.com>
2014-11-21 08:11:36 -0700

    updating imageMounter.py

--- scripts/imageMounter.py
+++ scripts/imageMounter.py
@@ -8,8 +8,8 @@ __description__ = 'Python script to MNT Partitions on a Disk Image'
 __author__ = 'Kevin Breen @KevTheHermit'
 __version__ = '0.5'
 __date__ = '2014/09/13'
-
-
+ 
+ 
 import os
 import sys
 import hashlib
@@ -17,12 +17,12 @@ import commands
 import subprocess
 from datetime import datetime
 from optparse import OptionParser, OptionGroup
-
+ 
 supported_types = ['0x83', '0x07', '0x0b', '0x06', '0x17', '0x16', '0x1b', 'Basic data partition']
 vfat = ['0x0b', '0x06', '0x1b', '0x16']
 ntfs = ['0x07', '0x17', 'Basic data partition']
-
-
+ 
+ 
 def parse_mmls(img_path):
     # use mmls to get a list of partitions.
     try:
@@ -62,13 +62,13 @@ def parse_mmls(img_path):
             partition_info[part_count] = inf
             part_count += 1
     return partition_info, part_count
-            
+             
 def mount_single(img_file, mnt_path):
     if not os.path.exists(mnt_path):
         os.makedirs(mnt_path)
     print "[+] Attempting to Mount {0} at {1}".format(img_file, mnt_path)
     try:
-        retcode = subprocess.call("mount -o ro,loop {0} {1}".format(img_file, mnt_path), shell=True)
+        retcode = subprocess.call("mount -o ro,loop,show_sys_files,streams_interface=windows {0} {1}" .format(img_file, mnt_path), shell=True)
         #Crappy error Handling here
         if retcode != 0:
             sys.exit()
@@ -76,14 +76,14 @@ def mount_single(img_file, mnt_path):
         print "   [-] To unmount run 'sudo umount %s'" % mnt_path
     except:
         print "[+] Failed to Mount %s" % mnt_path
-
+ 
 def mount_multi(img_file, mnt_path, part_count, part_data):
     for i in range(part_count):
         new_path = os.path.join(mnt_path, str(i))
         print "[+] Creating Temp Mount Point at {0}".format(new_path)
         if not os.path.exists(new_path):
             os.makedirs(new_path)
-        print "[+] Attempting to Mount Partition {0} at {1}".format(i, new_path) 
+        print "[+] Attempting to Mount Partition {0} at {1}".format(i, new_path)
         try:
             offset = part_data[i]["Offset"]
             fs_type = part_data[i]["Type"]
@@ -91,7 +91,7 @@ def mount_multi(img_file, mnt_path, part_count, part_data):
                 fs_type = 'vfat'
             if fs_type in ntfs:
                 fs_type = 'ntfs'
-            retcode = subprocess.call('mount -t {0} -o ro,loop,offset={1} {2} {3}'.format(fs_type, offset, img_file, new_path), shell=True)
+            retcode = subprocess.call('mount -t {0} -o ro,loop,show_sys_files,streams_interface=windows,offset={1} {2} {3}' .format(fs_type, offset, img_file, new_path), shell=True)
             #Crappy error Handling here
             if retcode != 0:
                 sys.exit()
@@ -99,7 +99,7 @@ def mount_multi(img_file, mnt_path, part_count, part_data):
             print "   [-] To unmount run 'sudo umount {0}'".format(new_path)
         except:
             print "[+] Failed to Mount {0}".format(new_path)
-
+ 
 def ewf_mount(img_file):
     print "[+] Processing E01 File"
     #check we have an EO file
@@ -124,7 +124,7 @@ def ewf_mount(img_file):
         except Exception as e:
             print "Failed to mount E0 File: {0}".format(e)
             sys.exit()
-
+ 
 def bitlock_mount(img_file, mnt_path, recover_key, part_data):
     # for now we only support recovery key
     offset = part_data[0]['Offset']
@@ -150,8 +150,8 @@ def bitlock_mount(img_file, mnt_path, recover_key, part_data):
         print "Failed to mount File: {0}".format(e)
         sys.exit()
     mount_single(img_path, mnt_path)
-
-
+ 
+ 
 def main():
     parser = OptionParser(usage='usage: %prog [options] image_name mnt_point\n' + __description__, version='%prog ' + __version__)
     parser.add_option("-s", "--single", action='store_true', default=False, help="Single partition in image")
@@ -160,47 +160,47 @@ def main():
     parser.add_option("-b", "--bitlocker", action='store_true', default=False, help="Mount Bitlocker Drive with Recovery Key")
     parser.add_option("-k", "--key", dest='recover_key', default=False, help="Bitlokcer Recovery Key")
     (options, args) = parser.parse_args()
-
+ 
     if len(args) == 0:
         print "[+] You need to give me some Paths"
         parser.print_help()
         sys.exit()
-
+ 
     # Sort args
     img_file = args[0]
     mnt_path = args[1]
-    
+     
     # Dont trust the user force E01 Check
     if img_file.endswith('.E01'):
         options.e01 = True
-    
+     
     # We need to proces E0 Files before anythign else
     if options.e01:
         # mount the ewf and use this as our new img_file
         img_file = ewf_mount(img_file)
-
+ 
     # Single partition. We have no mmls data so jsut try mounting
     if options.single:
         mount_single(img_file, mnt_path)
-    
+     
     # Mount a Disk with multi parts
     if not options.single and not options.bitlocker:
         # Get partition Information
         part_data, part_count = parse_mmls(img_file)
         if part_data:
             mount_multi(img_file, mnt_path, part_count, part_data)
-
+ 
     # Mount a BitLocked Drive
     if options.bitlocker and options.recover_key:
         part_data, part_count = parse_mmls(img_file)
         bitlock_mount(img_file, mnt_path, options.recover_key, part_data)
-
-
+ 
+ 
 if __name__ == "__main__":
     if os.getuid() == 0:
         main()
     else:
         print "[+] You must be Root or Sudo to run this Script"
         sys.exit()
-            
+             
             
